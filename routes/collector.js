16a263a2b796e9f0812700166448eae5307ffca6
diedsmiling <diedsmiling@gmail.com>
2015-10-21 23:36:07 +0300

    URL validation

--- routes/collector.js
+++ routes/collector.js
@@ -1,10 +1,27 @@
 'use strict';
-let requrest    = require('request');
+let request    = require('request');
 let validator   = require('validator');
 
 let collector = {
     collect: (req, res) => {
-        console.log(req.query.url);
+        let url      = req.query.url;
+        let vOptions = {
+            protocols: ['http', 'https']
+        };
+
+        if (typeof url == 'undefined' || url == '') {
+            res.json({
+                status: '400',
+                error:  'An empty url is not an url at all :('
+            });
+        }
+
+        if (!validator.isURL(url, vOptions)) {
+            res.json({
+                status: '400',
+                error:  'Wrong url :('
+            });
+        }
         res.json({res: 'Collect selectors'});
     }
 }
--- tests/backend/routes/collector.js
+++ tests/backend/routes/collector.js
@@ -1,11 +1,47 @@
 'use strict';
-var collector = require('../../../routes/collector');
+let collector = require('../../../routes/collector');
+let req;
+let res;
+let errorObj;
 
 describe('Collector', () => {
 
     describe('collect() method', () => {
+        beforeEach(() => {
+            res = {
+                json: sinon.spy()
+            };
+
+            req = {
+                query: {
+                    url: ''
+                }
+            };
+
+            errorObj = {
+                status: '400',
+                error:  ''
+            };
+        });
+
         it('should be defined', () => {
             expect(collector.collect).to.not.be.undefined;
+        });
+
+        it('should respond with an error if url is empty', () => {
+            errorObj.error = 'An empty url is not an url at all :(';
+            console.log(errorObj);
+
+            collector.collect(req, res);
+            expect(res.json).to.be.calledWith(errorObj);
+        });
+
+        it('should respond with an error if url is invalid', () => {
+            req.query.url  = 'WRONG_URL';
+            errorObj.error = 'Wrong url :(';
+
+            collector.collect(req, res);
+            expect(res.json).to.be.calledWith(errorObj);
         });
 
     });
