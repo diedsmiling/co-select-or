53511350ebc878dda1bed89d8696e5a7a47de8c3
diedsmiling <diedsmiling@gmail.com>
2015-10-21 22:34:49 +0300

    "request" module installed

--- routes/collector.js
+++ routes/collector.js
@@ -1,7 +1,9 @@
 'use strict';
+let requrest = require('request');
 
 let collector = {
     collect: (req, res) => {
+        console.log(req);
         res.json({res: 'Collect selectors'});
     }
 }
