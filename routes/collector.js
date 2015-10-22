2cd46197a76b4ef172577f24162292bb58eb272f
diedsmiling <diedsmiling@gmail.com>
2015-10-23 00:25:01 +0300

    Moved validation to separate validation() method

--- routes/collector.js
+++ routes/collector.js
@@ -3,35 +3,38 @@ let request    = require('request');
 let validator   = require('validator');
 
 class Collector {
-
-    collect(req, res) {
-        if (typeof req == 'undefined') {
-            return false;
-        }
-        let url      = req.query.url;
+    validate(url) {
         let vOptions = {
             protocols: ['http', 'https']
         };
 
         if (typeof url == 'undefined' || url == '') {
-            res.status(400);
-            res.json({
-                status: '400',
-                error:  'An empty url is not an url at all :('
-            });
-            return false;
+            return [false, 400, 'An empty url is not an url at all :('];
         }
 
         if (!validator.isURL(url, vOptions)) {
-            res.status(400);
+            return [false, 400, 'Wrong url :('];
+        }
+
+        return [true, 200];
+    }
+    collect(req, res) {
+        if (typeof req == 'undefined') {
+            return false;
+        }
+
+        let url      = req.query.url;
+        let [isValid, code, msg] = this.validate(url);
+
+        if (!isValid) {
+            res.status(code);
             res.json({
-                status: '400',
-                error:  'Wrong url :('
+                status:  code,
+                error:  msg
             });
             return false;
         }
     }
-
 }
 
 module.exports = Collector;
--- tests/backend/routes/collector.js
+++ tests/backend/routes/collector.js
@@ -9,36 +9,48 @@ let requestStub;
 
 describe('Collector', () => {
 
-    describe('collect() method', () => {
+    beforeEach(() => {
         requestStub = sinon.stub();
 
         Collector = proxyquire('../../../routes/collector', {
             request: requestStub
         });
-        beforeEach(() => {
-            res = {
-                json:   sinon.spy(),
-                status: sinon.spy()
-            };
 
-            req = {
-                query: {
-                    url: ''
-                }
-            };
+        res = {
+            json:   sinon.spy(),
+            status: sinon.spy()
+        };
 
-            errorObj = {
-                status: '400',
-                error:  ''
-            };
+        req = {
+            query: {
+                url: ''
+            }
+        };
 
-            collector = new Collector();
-        });
+        errorObj = {
+            status: 400,
+            error:  ''
+        };
+
+        collector = new Collector();
+    });
+
+    describe('collect() method', () => {
 
         it('should be defined', () => {
             expect(collector.collect).to.not.be.undefined;
         });
 
+        it('should validate url', () => {
+            sinon.spy(collector, 'validate');
+
+            collector.collect(req, res);
+            expect(collector.validate).to.be.called;
+        });
+
+    });
+
+    describe('validate() method', ()=> {
         it('should respond with an error if url is empty', () => {
             errorObj.error = 'An empty url is not an url at all :(';
 
@@ -55,7 +67,6 @@ describe('Collector', () => {
             expect(res.status).to.be.calledWith(400);
             expect(res.json).to.be.calledWith(errorObj);
         });
-
     });
 
 });
